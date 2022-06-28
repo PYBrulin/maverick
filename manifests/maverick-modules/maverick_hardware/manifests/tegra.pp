3c5fd41873676f062fece5c4345f7abf1d78243b
Pierre-Yves BRULIN <pierre-yves.brulin@estaca.fr>
2022-06-28 10:46:53 +0200

    Small changes to nvgetty condition and xaviernx localconf

--- manifests/maverick-modules/maverick_hardware/manifests/tegra.pp
+++ manifests/maverick-modules/maverick_hardware/manifests/tegra.pp
@@ -136,8 +136,8 @@ class maverick_hardware::tegra (
 
     if $tegra_model == "Jetson Xavier NX" {
         # Disables the nvgetty service which occupies the UART of the J12 header for the flight controller
-        exec { "disable nvgetty":
-            command     => "/bin/systemctl stop nvgetty; sleep 10; /bin/systemctl disable nvgetty;",
+        exec { "tegra-disable-nvgetty":
+            command     => "/bin/systemctl disable nvgetty;",
             onlyif      => "/bin/systemctl is-enabled nvgetty",
         }
     }
